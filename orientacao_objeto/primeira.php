b50ecab235cc04269b000cdcc96c8b25375383bc
Vitor Esteves <vitordasilvaesteves@gmail.com>
2022-01-15 11:35:39 -0300

    tipo de atributo - public e private

--- orientacao_objeto/primeira.php
+++ orientacao_objeto/primeira.php
@@ -2,13 +2,18 @@
 
 class Pessoa {
     // aqui ponho os ATRIBUTOS
-    public $nome = "vitor";
-    public $idade = "35";
-
+    private $nome;
+    private $idade;
 
     // aqui ponho os MÉTODOS
     public function correr() {
-        $this->validacao();
+        $validacao = $this->validacao();
+        if ($validacao) {
+            echo $validacao;
+            echo "<br />";
+            return;
+        }
+        
         return $this->nome . " está correndo";
     }
 
@@ -19,18 +24,37 @@ class Pessoa {
     public function validacao() {
         // fazer uma leitura do nome;
         // se o nome da pessoa começar com a letra v, a pessoa não poderá correr.
+        if(empty($this->nome)) {
+            return "informe um nome de pessoa";
+        }
+
+        echo "validando o nome " . $this->nome;
+        echo "<br />";
+
+        if (strtoupper($this->nome[0]) == "V") {
+            return $this->nome . " não pode correr";
+        }
+    }
+
+    // método responsável em pegar informação de dentro da classe
+    public function getNome(){
+        return $this->nome;
+    }
+
+    // método responsável em atribuir um dado externo para ser usado internamente na classe
+    public function setNome($nome){
+        $this->nome = $nome;
     }
 }
 
+// instanciando a pessoa 1
+// criando um registro de Pessoa
 $pessoa1 = new Pessoa();
+$pessoa1->setNome("Dandara");
+echo $pessoa1->getNome();
 
-// $pessoa1->nome = "Dandara";
-// echo $pessoa1->nome;
+echo "<br />";
 
 $pessoa2 = new Pessoa();
-// $pessoa2->nome = "Raph";
-echo "Aluno: " . $pessoa1->andar();
-echo "<br>";
-echo "Aluno " . $pessoa2->andar();
-echo "<br>";
-echo "<br>";
+$pessoa2->setNome("Leo");
+echo $pessoa2->getNome();
