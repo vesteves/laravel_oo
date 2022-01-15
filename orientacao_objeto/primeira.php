2d5a6b012566f346da7183b373d05ccfa4164ea1
Vitor Esteves <vitordasilvaesteves@gmail.com>
2022-01-15 12:11:06 -0300

    Final de aula de classes básicas

--- orientacao_objeto/primeira.php
+++ orientacao_objeto/primeira.php
@@ -4,6 +4,14 @@ class Pessoa {
     // aqui ponho os ATRIBUTOS
     private $nome;
     private $idade;
+    private $sexo;
+    private $estado;
+    private $cidade;
+    
+    // public e depois como privado
+    // sexo
+    // estado
+    // cidade
 
     // aqui ponho os MÉTODOS
     public function correr() {
@@ -31,30 +39,69 @@ class Pessoa {
         echo "validando o nome " . $this->nome;
         echo "<br />";
 
-        if (strtoupper($this->nome[0]) == "V") {
+        if ($this->nome[0] == "V") {
             return $this->nome . " não pode correr";
         }
     }
 
     // método responsável em pegar informação de dentro da classe
     public function getNome(){
-        return $this->nome;
+        $nome = strtoupper($this->nome);
+        return $nome;
     }
 
     // método responsável em atribuir um dado externo para ser usado internamente na classe
     public function setNome($nome){
         $this->nome = $nome;
     }
+
+    public function getSexo(){
+        return $this->sexo;
+    }
+
+    public function setSexo($sexo){
+        $this->sexo = $sexo;
+    }
+
+    public function getEstado(){
+        return $this->estado;
+    }
+
+    public function setEstado($estado){
+        $this->estado = $estado;
+    }
+
+    public function getCidade(){
+        return $this->cidade;
+    }
+
+    public function setCidade($cidade){
+        $this->cidade = $cidade;
+    }
 }
 
 // instanciando a pessoa 1
 // criando um registro de Pessoa
 $pessoa1 = new Pessoa();
-$pessoa1->setNome("Dandara");
+$pessoa1->setNome("Vitor");
+$pessoa1->setEstado("Rio de Janeiro");
+$pessoa1->setCidade("RJ");
+
+echo $pessoa1->correr();
+echo "<br />";
 echo $pessoa1->getNome();
+
+/*
+echo $pessoa1->getNome();
+echo "<br />";
+echo $pessoa1->getCidade();
+echo "<br />";
+echo $pessoa1->getEstado();
+echo "<br />";
 
 echo "<br />";
 
 $pessoa2 = new Pessoa();
 $pessoa2->setNome("Leo");
 echo $pessoa2->getNome();
+*/
